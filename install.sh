d00f7762e949402a30bcd7ce67c54c6d4ec804b4
Nicolas Borboën <ponsfrilus@gmail.com>
2026-03-18 11:58:56 +0100

    [feature] Add update & upgrade

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 # Usage: bash <(curl -s https://gist.githubusercontent.com/ponsfrilus/970db330c857285e40bb04954e554965/raw/install.sh)
 
 echo "Installation script for trainee latptop"
+
+# Update everything
+apt update && apt upgrade -y
+
