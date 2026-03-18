6968587d48dad6dfb210090a26d549b5763c2777
Nicolas Borboën <ponsfrilus@gmail.com>
2026-03-18 15:59:27 +0100

    [fix] No more sudo

--- install.sh
+++ install.sh
@@ -4,9 +4,17 @@
 # - bash <(curl -s https://raw.githubusercontent.com/epfl-fsd/config-laptop-stagiaire/main/install.sh)
 # - wget -O - https://raw.githubusercontent.com/epfl-fsd/config-laptop-stagiaire/main/install.sh | bash
 # Note: use $(cat /proc/sys/kernel/random/uuid | cut -d'-' -f1) to bypass GitHub cache
+
+# Ensure script ran as root
+if [ "$EUID" -ne 0 ]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
+# User account data
 NEW_USER="stage"
-# set passwore more than 8
-NEW_PASSWORD="superpassword"
+NEW_PASSWORD="superpassword" # 8 chars min
+
 echo "Installation script for trainee latptop"
 
 # Update everything
@@ -18,9 +26,9 @@ nmcli d wifi connect eduroam
 # Codium archive repo
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
   | gpg --dearmor \
-  | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
+  | dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
 echo -e 'Types: deb\nURIs: https://download.vscodium.com/debs\nSuites: vscodium\nComponents: main\nArchitectures: amd64 arm64\nSigned-by: /usr/share/keyrings/vscodium-archive-keyring.gpg' \
-  | sudo tee /etc/apt/sources.list.d/vscodium.sources
+  | tee /etc/apt/sources.list.d/vscodium.sources
 
 # Re-update for new repos
 apt update
@@ -41,29 +49,28 @@ apt install -y \
 
 # Install Docker
 # See https://docs.docker.com/engine/install/ubuntu/
+# TODO: do not install Docker if `docker` is already working
 curl -fsSL https://get.docker.com -o get-docker.sh
 sh ./get-docker.sh
 
 # Docker post-install (https://docs.docker.com/engine/install/linux-postinstall/)
-sudo groupadd docker 2>/dev/null || true
-sudo usermod -aG docker administrator
+groupadd docker 2>/dev/null || true
+usermod -aG docker administrator
 
-# systemctl enable docker.service
-# systemctl enable containerd.service
 # Add a new user
 if ! id "$NEW_USER" &>/dev/null; then
-  sudo useradd -m -s /bin/bash "$NEW_USER"
+  useradd -m -s /bin/bash "$NEW_USER"
 fi
-echo "$NEW_USER:$NEW_PASSWORD" | sudo chpasswd
-sudo usermod -aG docker "$NEW_USER"
+echo "$NEW_USER:$NEW_PASSWORD" | chpasswd
+usermod -aG docker "$NEW_USER"
 
-sudo sed -i 's/^.*AutomaticLoginEnable = .*/AutomaticLoginEnable = true/' /etc/gdm3/custom.conf
-sudo sed -i "s/^.*AutomaticLogin = .*/AutomaticLogin = $NEW_USER/" /etc/gdm3/custom.conf
+sed -i 's/^.*AutomaticLoginEnable = .*/AutomaticLoginEnable = true/' /etc/gdm3/custom.conf
+sed -i "s/^.*AutomaticLogin = .*/AutomaticLogin = $NEW_USER/" /etc/gdm3/custom.conf
 
 # Ensure the challenge is running
 # See https://github.com/lvenries/stage_challenge
-sudo docker rm -f stage-challenge || true
-sudo docker run -d \
+docker rm -f stage-challenge || true
+docker run -d \
   -p 80:80 \
   -p 2222:22 \
   --restart always \
