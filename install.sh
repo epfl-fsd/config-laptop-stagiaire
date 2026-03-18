a6a5ded0e1a02416946440902ed83a2170615344
Nicolas Borboën <ponsfrilus@gmail.com>
2026-03-18 11:58:40 +0100

    [feature] Add source and usage

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
+# Source: https://gist.github.com/ponsfrilus/970db330c857285e40bb04954e554965
+# Usage: bash <(curl -s https://gist.githubusercontent.com/ponsfrilus/970db330c857285e40bb04954e554965/raw/install.sh)
 
 echo "Installation script for trainee latptop"
